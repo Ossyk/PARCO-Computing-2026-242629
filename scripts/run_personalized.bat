0dd9f6251870e3c9a63dcf79ec4a529c29ef3b29
Oussema Kasraoui <oussema.kasraoui@studenti.unitn.it>
2025-11-16 12:56:40 +0100

    Update run_personalized.bat

--- scripts/run_personalized.bat
+++ scripts/run_personalized.bat
@@ -41,6 +41,8 @@ echo --- Testing thread scaling ---
 %EXE% 8 %SCHEDULER% %CHUNKS% %MATRIX%
 %EXE% 16 %SCHEDULER% %CHUNKS% %MATRIX%
 %EXE% 32 %SCHEDULER% %CHUNKS% %MATRIX%
+%EXE% 64 %SCHEDULER% %CHUNKS% %MATRIX%
+
 echo.
 
 :: Test different schedules with 4 threads
@@ -65,3 +67,4 @@ echo Benchmarks Complete
 echo ===================================
 
 pause
+
