958fc0b281a20f8393914f3c41929f4391bdc020
Oussema Kasraoui <oussema.kasraoui@studenti.unitn.it>
2026-01-05 13:06:03 +0100

    clean struct pushed without large matrices + webbase

--- D2-242629/scripts/add_missing_matrices.sh
+++ D2-242629/scripts/add_missing_matrices.sh
@@ -8,11 +8,19 @@
 ./src/random_matrix_generator 16000 10 1 matrices/randomN320k.mtx
 
 cd matrices
+
 wget http://sparse-files.engr.tamu.edu/MM/Janna/Flan_1565.tar.gz
 tar -xzf Flan_1565.tar.gz
 mv Flan_1565/Flan_1565.mtx .
 rm -rf Flan_1565/
 rm Flan_1565.tar.gz
+
+wget http://sparse-files.engr.tamu.edu/MM/Williams/webbase-1M.tar.gz
+tar -xzf webbase-1M.tar.gz
+mv webbase-1M/webbase-1M.mtx .
+rm -rf webbase-1M/
+rm webbase-1M.tar.gz
+
 cd ..
 
 echo "matrices are ready to be used"
