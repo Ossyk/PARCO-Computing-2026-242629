a0b1bb61a436c4300bc85051c27c30fc0ef05f27
Oussema Kasraoui <oussema.kasraoui@studenti.unitn.it>
2026-01-05 13:53:17 +0100

    updating struct

--- D2-242629/scripts/strong_scaling.sh
+++ D2-242629/scripts/strong_scaling.sh
@@ -16,7 +16,7 @@ echo "*************************"
 prs=("1" "2" "4" "8" "16" "32" "64" "128") 
 for p in "${prs[@]}"; do
   echo "Nb of processes : $p" 
-  mpirun -np $p ./src/spmv_mpi_parallelIO ./matrices/$matrix
+  mpirun -np $p ./src/spmv_mpi_parallelIO $matrix
   echo 
 done
 
@@ -27,7 +27,7 @@ echo "*************************"
 prs=("1" "2" "4" "8" "16" "32" "64" "128") 
 for p in "${prs[@]}"; do
   echo "Nb of processes : $p" 
-  mpirun -np $p ./src/spmv_mpi_2d_parallelIO ./matrices/$matrix
+  mpirun -np $p ./src/spmv_mpi_2d_parallelIO $matrix
   echo 
 done
 
